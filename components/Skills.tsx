4b856514893651375cfa86eced7845c5d223c3c8
Mateusz Sodolski <mateuszsodolskiofficial@gmail.com>
2023-10-03 15:32:18 +0200

    Added Skills idk why tailwind isnt working for some reason

---
+++ components/Skills.tsx
@@ -0,0 +1,10 @@
+export interface SkillsInterface {
+  skillLevel: string;
+}
+export const Skills: React.FC<SkillsInterface> = ({ skillLevel }) => {
+  return (
+    <fieldset className="bg-blue-300 px-2 py-1 hover:brightness-125">
+      <legend className="text-center">{skillLevel}</legend>
+    </fieldset>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import { AboutMe } from "components/AboutMe";
 import { Picture } from "components/Picture";
+import { Skills, SkillsInterface } from "components/Skills";
 import Head from "next/head";
-
+const skillsArray: SkillsInterface[] = [
+  { skillLevel: "Well known and used in multiple of my projects" },
+  { skillLevel: "Know the basics but didn't use it that much" },
+  { skillLevel: "Want to learn" },
+];
 export default function Home() {
   return (
     <>
@@ -13,6 +18,9 @@ export default function Home() {
       <main className="flex min-h-screen  flex-col items-center gap-4 bg-gradient-to-b from-[#2e026d] to-[#15162c] px-[10vw] pt-2  text-center text-white">
         <Picture />
         <AboutMe />
+        {skillsArray.map((x) => (
+          <Skills {...x} key={x.skillLevel} />
+        ))}
       </main>
     </>
   );
